d63d52a4dc8afe27c0b5ececa0f26882f90f0b76
Daniel Choi <dhchoi@gmail.com>
2013-02-22 10:42:53 -0500

    show tweets to #railsbridge on workshop info page

--- public/js/application.js
+++ public/js/application.js
@@ -1,6 +1,61 @@
 
-(function($) {
+function tweetTimeStamp(createdAtStr) {
+    var tTime=new Date(Date.parse(createdAtStr));
+    var cTime=new Date();
+    var sinceMin=Math.round((cTime-tTime)/60000);
+    if(sinceMin==0){
+      var sinceSec=Math.round((cTime-tTime)/1000);
+      if(sinceSec<10)
+        var since='less than 10 seconds ago';
+      else if(sinceSec<20)
+        var since='less than 20 seconds ago';
+      else
+        var since='half a minute ago';
+    }
+    else if(sinceMin==1){
+      var sinceSec=Math.round((cTime-tTime)/1000);
+      if(sinceSec==30)
+        var since='half a minute ago';
+      else if(sinceSec<60)
+        var since='less than a minute ago';
+      else
+        var since='1 minute ago';
+    }
+    else if(sinceMin<45)
+      var since=sinceMin+' minutes ago';
+    else if(sinceMin>44 && sinceMin<60)
+      var since='about 1 hour ago';
+    else if(sinceMin<1440){
+      var sinceHr=Math.round(sinceMin/60);
+      if(sinceHr==1)
+        var since='about 1 hour ago';
+      else
+        var since='about '+sinceHr+' hours ago';
+    }
+    else if(sinceMin>1439 && sinceMin<2880)
+      var since='1 day ago';
+    else{
+      var sinceDay=Math.round(sinceMin/1440);
+      var since=sinceDay+' days ago';
+    }
+    return since;
+}
 
+function getTweets(query) {
+  var q = escape(query);
+  console.log(q);
+  $.getJSON("http://search.twitter.com/search.json?q="+q+"&rpp=12&callback=?", function(data) {     
+    console.log(data);
+    $(data.results).each(function(i,v) { 
+      var tweetHtml = ich.tweetTemplate(v);
+      console.log(v); 
+      $("#tweetStream").before(tweetHtml);
+    });
+  });
+};
+
+
+(function($) {
 
 
   $(function() {
@@ -11,6 +66,11 @@
 
 
   $(document).ready(function(){
+
+    if ( $("#tweetStream")[0] ) getTweets("#railsbridge");
+
+
+
 
     if (location.href.search("/curriculum") !== -1 &&
         location.href.search("/curriculum_toc") === -1 
