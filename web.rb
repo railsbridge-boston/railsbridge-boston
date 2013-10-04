6daef4591ef356e95aa673ae2600ebf5e6d10cf6
Daniel Choi <dhchoi@gmail.com>
2013-10-04 08:40:37 -0400

    fix total_students count

--- web.rb
+++ web.rb
@@ -113,8 +113,8 @@ class RubyWorkshop < Sinatra::Base
   get '/completions' do
     completions = DB["select page, count(student_id) from completions inner join students using (student_id) \ 
        where students.name is not null and workshop = ? group by page", CURRENT_WORKSHOP].to_a
-    total_students = DB["select count(*) as total from completions inner join students using (student_id) \
-       where students.name is not null and workshop = ?  group by student_id", CURRENT_WORKSHOP].first[:total]
+    total_students = DB["select count(*) from students 
+       where students.name is not null and workshop = ? ", CURRENT_WORKSHOP].first[:total]
     total_completions = DB["select count(*) as total from completions inner join students using (student_id) \
        where students.name is not null and workshop = ?", CURRENT_WORKSHOP].first[:total]
     {completions: completions, total_students: total_students, total_completions: total_completions }.to_json
